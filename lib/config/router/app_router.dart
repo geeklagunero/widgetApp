6fa7995d48805dcbb9f08b7398a6daaeba99f8c8
Ricardo Roman Landeros <ricardomon85@gmail.com>
2025-02-06 15:19:34 -0600

    progress indicator

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:widget_app/presentation/screens/buttons/buttons_screens.dart';
 import 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widget_app/presentation/screens/home/home_screen.dart';
+import 'package:widget_app/presentation/screens/progress/progress_screen.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
@@ -21,6 +22,11 @@ final appRouter = GoRouter(
       path: '/cards',
       name: CardsScreens.name,
       builder: (context, state) => CardsScreens(),
+    ),
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => ProgressScreen(),
     ),
   ],
 );
--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -6,6 +6,53 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress Indicator'),
+      ),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  //const _ProgressView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Text('Circular progress indicator'),
+          const SizedBox(
+            height: 15,
+          ),
+          CircularProgressIndicator(
+            strokeWidth: 4,
+            backgroundColor: Colors.black45,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text('Circular progress indicator controlado'),
+          const SizedBox(
+            height: 15,
+          ),
+          _ControllerProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  //const _ControllerProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
