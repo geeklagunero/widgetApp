a207aefcce10d6638bb35a5d02a772a178015aa9
Ricardo Roman Landeros <ricardomon85@gmail.com>
2025-02-06 14:34:45 -0600

    agregar archivo de barril

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+import 'package:widget_app/presentation/screens/animated/animated_screen.dart';
+import 'package:widget_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+import 'package:widget_app/presentation/screens/infinite_scroll/inifinite_scroll_screen.dart';
+import 'package:widget_app/presentation/screens/progress/progress_screen.dart';
+import 'package:widget_app/presentation/screens/snack_bar/snackbar_screen.dart';
+import 'package:widget_app/presentation/screens/ui_controls/ui_controls_screen.dart';
+
 export 'package:widget_app/presentation/screens/buttons/buttons_screens.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
